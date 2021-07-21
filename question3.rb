5686d1faf9d2b57e2245cfdc0fc5b4fca5c27c19
T-o-s-s-h-y <tosshy_1219@outlook.jp>
2021-07-22 08:33:11 +0900

    add question

---
+++ question3.rb
@@ -0,0 +1,17 @@
+=begin
+  p×q=177773となる素数pとqを求めよ。(p<q)
+=end
+
+require 'prime'
+
+p = 0
+q = 0
+target = 177_773
+Math.sqrt(target).to_i.times { |num|
+  next unless Prime.prime?(num) && (target % num).zero?
+
+  p = num
+  q = target / num
+  break if Prime.prime?(p) && Prime.prime?(q)
+}
+puts "p=#{p}, q=#{q}"
