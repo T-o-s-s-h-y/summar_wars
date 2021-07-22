d2779bbcf5d74a2e7a66d5787aff8902c0c76f19
T-o-s-s-h-y <tosshy_1219@outlook.jp>
2021-07-22 15:33:39 +0900

    fix rubocop

--- question3.rb
+++ question3.rb
@@ -5,11 +5,12 @@ require 'prime'
 p = 0
 q = 0
 target = 177_773
-Math.sqrt(target).to_i.times { |num|
+Math.sqrt(target).to_i.times do |num|
   next unless Prime.prime?(num) && (target % num).zero?
 
   p = num
   q = target / num
   break if Prime.prime?(p) && Prime.prime?(q)
-}
+end
+
 puts "p=#{p}, q=#{q}"
